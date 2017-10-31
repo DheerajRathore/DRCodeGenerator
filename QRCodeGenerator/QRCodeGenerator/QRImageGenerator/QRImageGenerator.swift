6ffc6f7da12e67c81590c3c230e8d7b95fbe3cc5
DheerajRathore <rathoredheeraj7@gmail.com>
2017-10-31 22:29:11 +0530

    Comment added for  generateQRImage.

--- QRCodeGenerator/QRCodeGenerator/QRImageGenerator/QRImageGenerator.swift
+++ QRCodeGenerator/QRCodeGenerator/QRImageGenerator/QRImageGenerator.swift
@@ -16,34 +16,33 @@ class QRImageGenerator: NSObject {
     private var qrcodeCIImage: CIImage!
     
     /**
-        Create QRcode from the text.
-       -parameters:
-            takes an option string whose QRCode needs to be generated
-       -return:
-            UIimage object is returned which contain's QR code for the text.
-    */
+     Create QRcode from the text.
+     -parameters:
+     takes an option string whose QRCode needs to be generated
+     -return:
+     UIimage object is returned which contain's QR code for the text.
+     */
     func generateQRImage(text : String) -> UIImage {
         
+        ///Convert text to data
+        let data = text.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
         
-            ///Convert text to data
-            let data = text.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
-            
-            ///Add filters to generate QRImage.
-            let filter = CIFilter(name: "CIQRCodeGenerator")
-            filter?.setValue(data, forKey: "inputMessage")
-            filter?.setValue("Q", forKey: "inputCorrectionLevel")
-            
-            ///Get CIImage
-            qrcodeCIImage = filter?.outputImage
-           
-            ///Get UIImage object from CIImage
-           // qrCodeImage = UIImage.init(cgImage: qrcodeCIImage as! CGImage)
-            
-            let context:CIContext = CIContext.init(options: nil)
-            let cgImage:CGImage = context.createCGImage(qrcodeCIImage, from: qrcodeCIImage.extent)!
-            qrCodeImage = UIImage.init(cgImage: cgImage)
-            
-            ///return qr code image.
+        ///Add filters to generate QRImage.
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        filter?.setValue(data, forKey: "inputMessage")
+        filter?.setValue("Q", forKey: "inputCorrectionLevel")
+        
+        ///Get CIImage
+        qrcodeCIImage = filter?.outputImage
+        
+        ///Get UIImage object from CIImage
+        ///qrCodeImage = UIImage.init(cgImage: qrcodeCIImage as! CGImage)
+        
+        let context:CIContext = CIContext.init(options: nil)
+        let cgImage:CGImage = context.createCGImage(qrcodeCIImage, from: qrcodeCIImage.extent)!
+        qrCodeImage = UIImage.init(cgImage: cgImage)
+        
+        ///return qr code image.
         return qrCodeImage!
         
     }
